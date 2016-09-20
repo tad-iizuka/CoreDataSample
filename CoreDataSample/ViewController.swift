8cd98e54369b579379ac544014163ac31f755bb6
tad-iizuka <tad.iizuka@gmail.com>
2016-09-20 21:17:49 +0900

    Initial Commit

---
+++ CoreDataSample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CoreDataSample
+//
+//  Created by Tadashi on 9/20/16.
+//  Copyright © 2016 T@d. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
